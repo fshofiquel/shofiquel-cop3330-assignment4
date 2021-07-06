3c20dfc83d25a55c1353bb716fc8bd35a260485f
fshofiquel <fshofique@gmail.com>
2021-07-05 20:46:35 -0400

    Added testcases

---
+++ src/main/java/ucf/assignments/createTodoList.java
@@ -0,0 +1,8 @@
+package ucf.assignments;
+
+public class createTodoList
+{
+	// This class will be meant to help prevent clutting of the driver class by creating/deleting every new todolist
+	// as well as creating/editing/deleting the lists in them
+	// Utilization will be using ArrayLists and properly removing them.
+}
