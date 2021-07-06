13a0c34c65ef757ed5f75cea0ee8d4eb42567a5b
fshofiquel <fshofique@gmail.com>
2021-07-05 20:28:26 -0400

    Added tests

---
+++ src/main/java/ucf/assignments/ToDoListController.java
@@ -0,0 +1,53 @@
+package ucf.assignments;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+public class ToDoListController
+{
+
+	@FXML
+	public ListView toDosList;
+	@FXML
+	public TextField toDoListName;
+	@FXML
+	public ListView tasksList;
+	@FXML
+	public TextField editTasks;
+
+	@FXML
+	public void addToDoClick(ActionEvent actionEvent)
+	{
+	}
+
+	@FXML
+	public void exportAllClick(ActionEvent actionEvent)
+	{
+	}
+
+	@FXML
+	public void exportListClick(ActionEvent actionEvent)
+	{
+	}
+
+	@FXML
+	public void removeTodoClick(ActionEvent actionEvent)
+	{
+	}
+
+	@FXML
+	public void addOrEditClick(ActionEvent actionEvent)
+	{
+	}
+
+	@FXML
+	public void viewClick(ActionEvent actionEvent)
+	{
+	}
+
+
+
+}
---
+++ src/test/java/ucf/assignments/ToDoListControllerTest.java
@@ -0,0 +1,49 @@
+package ucf.assignments;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ToDoListControllerTest
+{
+
+	@Test
+	void addToDoClick()
+	{
+		// Send text obtained from toDoName to toDoList
+		// Assert that they match with each other and the testcase
+	}
+
+	@Test
+	void exportAllClick()
+	{
+		// Confirm that an external file is created and both files match each other
+	}
+
+	@Test
+	void exportListClick()
+	{
+		// Confirm that an external file for a single list is created and both files match each other
+	}
+
+	@Test
+	void removeTodoClick()
+	{
+		// Removes ToDo list
+		// Confirms that there is nothing rememaing (both the list and the todo must be gone)
+	}
+
+	@Test
+	void addOrEditClick()
+	{
+		// Confirm the changes made to a list
+	}
+
+	@Test
+	void viewClick()
+	{
+		// Confirm that a new Todo list is being viewed.
+	}
+
+
+}
